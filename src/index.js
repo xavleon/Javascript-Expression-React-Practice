7747162a563d2358d1e5ddbd520de6be01ed5d29
xavleon <80280981+xavleon@users.noreply.github.com>
2022-04-28 14:37:34 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+//It should display 2 paragraph HTML elements.
+//The paragraphs should say:
+//Created by YOURNAME.
+//Copyright CURRENTYEAR.
+//E.g.
+//Created by Angela Yu.
+//Copyright 2019.
+
+ReactDOM.render(
+  <div>
+    <p>Created by Javier Leon</p>
+    <p>Copyright 2022</p>
+  </div>,
+  document.getElementById("root")
+);
